a7438cbb92b743fd81bc9ee91e3c03b20845a73c
Owen Utley <90651143+owenutley@users.noreply.github.com>
2024-01-10 12:19:12 -0700

    added new styling for page, toggle with button

---
+++ resources/scripts/index.js
@@ -0,0 +1,20 @@
+let stylebtn = document.getElementsByClassName('color-change')
+
+function toggleStyle() {
+    if (stylebtn.id == 'style1') {
+        document.documentElement.style.setProperty('--color-1', 'rgb(32, 14, 58)');
+        document.documentElement.style.setProperty('--color-2', 'rgb(56, 65, 157)');
+        document.documentElement.style.setProperty('--color-3', 'rgb(56, 135, 190)');
+        document.documentElement.style.setProperty('--color-4', 'rgb(82, 211, 216)');
+        document.documentElement.style.setProperty('--color-5', 'rgb(208, 245, 253)');
+        stylebtn.id = 'style2'
+    } else {
+        console.log("it failed sorry\n", )
+        document.documentElement.style.setProperty('--color-1', 'rgb(34, 9, 44)');
+        document.documentElement.style.setProperty('--color-2', 'rgb(135, 35, 65)');
+        document.documentElement.style.setProperty('--color-3', 'rgb(190, 49, 68)');
+        document.documentElement.style.setProperty('--color-4', 'rgb(240, 89, 65)');
+        document.documentElement.style.setProperty('--color-5', 'rgb(253, 226, 204)');
+        stylebtn.id = 'style1'
+    }
+};
