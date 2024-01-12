eda43de3582f8e566fa37c5196b0ac967193f273
Owen Utley <90651143+owenutley@users.noreply.github.com>
2024-01-12 15:38:12 -0700

    fixed style change for all pages

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,19 +1,35 @@
+window.addEventListener('DOMContentLoaded', storedStyle);
+
 let stylebtn = document.getElementsByClassName('color-change')[0];
 
-function toggleStyle() {
-    if (stylebtn.id === 'style1') {
+function applyStyle(style) {
+    if (style === 'style2') {
         document.documentElement.style.setProperty('--color-1', 'rgb(32, 14, 58)');
         document.documentElement.style.setProperty('--color-2', 'rgb(56, 65, 157)');
         document.documentElement.style.setProperty('--color-3', 'rgb(56, 135, 190)');
         document.documentElement.style.setProperty('--color-4', 'rgb(82, 211, 216)');
         document.documentElement.style.setProperty('--color-5', 'rgb(208, 245, 253)');
-        stylebtn.id = 'style2'
     } else {
         document.documentElement.style.setProperty('--color-1', 'rgb(34, 9, 44)');
         document.documentElement.style.setProperty('--color-2', 'rgb(135, 35, 65)');
         document.documentElement.style.setProperty('--color-3', 'rgb(190, 49, 68)');
         document.documentElement.style.setProperty('--color-4', 'rgb(240, 89, 65)');
         document.documentElement.style.setProperty('--color-5', 'rgb(253, 226, 204)');
-        stylebtn.id = 'style1'
-    }
+    };
 };
+
+function toggleStyle() {
+    const currentStyle = stylebtn.id;
+    const newStyle = currentStyle === 'style1' ? 'style2' : 'style1';
+    localStorage.setItem('currentStyle', newStyle);
+    stylebtn.id = newStyle;
+    applyStyle(newStyle);
+}
+
+function storedStyle() {
+    const storedStyle = localStorage.getItem('currentStyle');
+    if (storedStyle) {
+        stylebtn.id = storedStyle;
+        applyStyle(storedStyle);
+    }
+}
