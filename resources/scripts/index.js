6be9628947d8b6b32b8be581d684397476b26f1e
Owen Utley <90651143+owenutley@users.noreply.github.com>
2024-01-13 13:27:17 -0700

    added settings button

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -3,12 +3,18 @@ window.addEventListener('DOMContentLoaded', storedStyle);
 let stylebtn = document.getElementsByClassName('color-change')[0];
 
 function applyStyle(style) {
-    if (style === 'style2') {
+    if (style === 'redSt') {
         document.documentElement.style.setProperty('--color-1', 'rgb(32, 14, 58)');
         document.documentElement.style.setProperty('--color-2', 'rgb(56, 65, 157)');
         document.documentElement.style.setProperty('--color-3', 'rgb(56, 135, 190)');
         document.documentElement.style.setProperty('--color-4', 'rgb(82, 211, 216)');
         document.documentElement.style.setProperty('--color-5', 'rgb(208, 245, 253)');
+    } else if (style === 'greenSt') {
+        document.documentElement.style.setProperty('--color-1', 'rgb(15, 15, 15)');
+        document.documentElement.style.setProperty('--color-2', 'rgb(35, 45, 63)');
+        document.documentElement.style.setProperty('--color-3', 'rgb(0, 91, 65)');
+        document.documentElement.style.setProperty('--color-4', 'rgb(0, 129, 112)');
+        document.documentElement.style.setProperty('--color-5', 'rgb(125, 218, 212)');
     } else {
         document.documentElement.style.setProperty('--color-1', 'rgb(34, 9, 44)');
         document.documentElement.style.setProperty('--color-2', 'rgb(135, 35, 65)');
