25b87d3a132bb3e7ed701b8c05bfb00707a476c3
Owen Utley <owen.utley17@gmail.com>
2026-01-30 18:34:49 -0700

    added projects page to routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navber from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Projects from './pages/Projects';
 import './App.css'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   )
