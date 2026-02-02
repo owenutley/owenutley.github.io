e4cbed11fc5fd0c7995b42dd1146f8803cddb3c3
Owen Utley <owen.utley17@gmail.com>
2026-02-01 19:23:08 -0700

    profile caricature created and added to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
-
+import faceImg from "../assets/face-char.png";
+import "./Home.css";
 
 function Home() {
-    return <h2>Home Page Content</h2>;
+    return (
+        <main>
+            <h1>Welcome to the Home Page</h1>
+            <img src={faceImg} alt="Profile Face" className="profile-face"/>
+        </main>
+    );
 }
 
 export default Home;
